25a9749644584f4deee55b46f6cc53a7802b387a
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-17 14:41:50 +0300

    project files. storyboard with text and buttons.

---
+++ iOS Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Calculator
+//
+//  Created by Eetu Hernesniemi on 17.9.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
